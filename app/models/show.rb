0674e4f44ac0cbd0b2358d3d6512f590b91efe4e
Daniel Etter <etter.dt@gmail.com>
2018-11-09 16:44:24 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,23 @@
 class Show < ActiveRecord::Base
   
+  def Show::highest_rating
+    Show.maximum(:rating)
+  end
   
+  def Show::most_popular_show
+    Show.order(rating: :desc).first
+    # Show.highest_rating
+  end
   
+  def Show::lowest_rating
+    Show.minimum(:rating)
+  end
+  
+  def Show::least_popular_show
+    Show.order(rating: :asc).first
+  end
+  
+  def Show::ratings_sum
+    Show.sum("ratings")
+  end
 end
